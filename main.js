f4dc54318a53476b3082bab64282a49598f28369
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-03-11 19:20:52 +0330

    API added to this project

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function displayRegionInfo(event, element) {
     const regionName = element.getAttribute("name");
-    const regionInfo = element.getAttribute("name_en"); // تغییر در اینجا بر اساس نیاز
+    const regionInfo = element.getAttribute("code"); // تغییر در اینجا بر اساس نیاز
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
-    const apiUrl = "https://api.codebazan.ir/weather/?city=" + regionName;
+    const apiUrl = "https://prayer.aviny.com/api/prayertimes/" + regionInfo;
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -32,11 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then((data) => {
         // استفاده از اطلاعات دریافتی از API به عنوان regionInfo
-        const regionInfo = `Temperature: ${data.temperature}°C, Condition: ${data.condition}`;
+        const regionInfo = `مرکز استان: ${data.CityName}<br>تاریخ شمسی: ${data.Today}<br>تاریخ قمری: ${data.TodayQamari}<br>
+        اذان صبح: ${data.Imsaak}<br>طلوع آفتاب: ${data.Sunrise}<br>اذان ظهر: ${data.Noon}<br>
+        غروب آفتاب: ${data.Sunset}<br>اذان مغرب: ${data.Maghreb}<br>نیمه شب شرعی: ${data.Midnight}
+        <br>منطقه زمانی: ${data.TimeZone}`;
 
         infoBox.style.left = mouseX + "px";
         infoBox.style.top = mouseY + "px";
-        infoBox.innerHTML = `${regionName}<br>${regionInfo}`;
+        infoBox.innerHTML = `${regionName}<br><br>${regionInfo}`;
 
         setTimeout(() => {
           infoBox.classList.add("show");
@@ -47,3 +50,26 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 });
+
+// $(document).ready(function () {
+//   $.ajax({
+//     url: "https://prayer.aviny.com/api/prayertimes/1",
+//     dataType: "json",
+//     type: "GET",
+//     success: function (data) {
+//       $("#div_test").append("نام شهر: " + data.CityName + "<br/>");
+//       $("#div_test").append("نام کشور: " + data.CountryName + "<br/>");
+//       $("#div_test").append("امروز: " + data.Today + "<br/>");
+//       $("#div_test").append("" + data.TodayQamari + "<br/>");
+//       $("#div_test").append("اذان صبح: " + data.Imsaak + "<br/>");
+//       $("#div_test").append("طلوع آفتاب: " + data.Sunrise + "<br/>");
+//       $("#div_test").append("اذان ظهر: " + data.Noon + "<br/>");
+//       $("#div_test").append("غروب خورشید: " + data.Sunset + "<br/>");
+//       $("#div_test").append("اذان مغرب: " + data.Maghreb + "<br/>");
+//       $("#div_test").append("نیمه شب شرعی: " + data.Midnight + "<br/>");
+//       $("#div_test").append("نام شهر: " + data.CityLName + "<br/>");
+//       $("#div_test").append("نام کشور: " + data.CountryLName + "<br/>");
+//       $("#div_test").append("وقت محلی: " + data.TimeZone + "<br/>");
+//     },
+//   });
+// });
